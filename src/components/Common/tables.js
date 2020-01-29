bf3013242fdaf59696dd844bc1ed08af395b5acd
dagrel <dagrel03@gmail.com>
2020-01-29 16:29:38 +0100

    siste?

--- src/components/Common/tables.js
+++ src/components/Common/tables.js
@@ -1,43 +1,6 @@
 import React, { Component } from 'react';
 import { Router, Route, Link } from 'react-router-dom';
 
-
-
-/*export const contactsColumn = [
-    {
-        "label": "ID",
-        "name": "key",
-        "options": {
-            "filter": false,
-            "sort": false,
-            customBodyRender: (value, tableMeta, updateValue) => (
-                <Link to={`/kontaktdetaljer/${tableMeta.rowData[0]}`}>
-                    {value}
-                </Link>
-            )
-        }
-    },
-    {
-        "label": "Info ID",
-        "name": "ParentBusinessRelationID",
-        "options": {
-            "filter": false,
-            "sort": true,
-        }
-    },
-    {
-        "label": "Selskapsrelasjon",
-        "name": "InfoID",
-        "options": {
-            "filter": false,
-            "sort": true,
-        }
-    },
-    
-];
-*/
-
-
 export const assetsColumn = [
     {
         "label": "ID",
@@ -102,73 +65,5 @@ export const assetsColumn = [
             "sort": true
         }
     },
-
-    /*{
-        "label": "Beskrivelse",
-        "name": "description_long",
-        "options": {
-            "filter": false,
-            "sort": false,
-            "display": "excluded"
-        }
-    },
-    {
-        "label": "Teknisk beskrivelse",
-        "name": "description_technical",
-        "options": {
-            "filter": false,
-            "sort": false,
-            "display": "excluded"
-        }
-    },
-    {
-        "label": "Dato kondemnert",
-        "name": "date_scrapped",
-        "options": {
-            "filter": false,
-            "sort": true,
-           "display": "excluded"
-        }
-    },
-    {
-        "label": "Dato kjøpt",
-        "name": "date_bought",
-        "options": {
-            "filter": false,
-            "sort": true,
-        }
-    }, 
-    {
-        "label": "Type",
-        "name": "type_label",
-        "options": {
-            "filter": true,
-            "sort": true,
-        }
-    },
-    {
-        "label": "Variant",
-        "name": "variant_label",
-        "options": {
-            "filter": true,
-            "sort": true,
-        }
-    },
-    {
-        "label": "Verdi",
-        "name": "current_value",
-        "options": {
-            "filter": true,
-            "sort": true,
-        }
-    },
-    {
-        "label": "Original pris",
-        "name": "original_price",
-        "options": {
-            "filter": true,
-            "sort": true,
-        }
-    }*/
 ];
 
